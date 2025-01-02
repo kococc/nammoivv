21d868f16c542d38d14e22539d1d8afbed4ad23f
kococc <kophaiduongthai@gmail.com>
2025-01-02 23:29:47 +0700

    Update nammoivv.py

--- nammoivv.py
+++ nammoivv.py
@@ -1,24 +1,21 @@
-import time 
+import time
 from time import sleep
-from colorama import Fore, init
+from colorama import Fore, Style, init
 import random
 
-xnhac = "\033[1;36m"
-do = "\033[1;31m"
-luc = "\033[1;32m"
-vang = "\033[1;33m"
-xduong = "\033[1;34m"
-hong = "\033[1;35m"
-trang = "\033[1;37m"
-whiteb="\033[1;37m"
-red="\033[0;31m"
-redb="\033[1;31m"
-end='\033[0m'
 # Khởi tạo Colorama
 init(autoreset=True)
 
-# Danh sách các màu có sẵn trong colorama
-colors = [Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN, Fore.WHITE]
+# Danh sách các màu đậm và sáng hơn từ colorama
+colors = [
+    Style.BRIGHT + Fore.RED,       # Đỏ đậm
+    Style.BRIGHT + Fore.GREEN,     # Xanh lá đậm
+    Style.BRIGHT + Fore.YELLOW,    # Vàng đậm
+    Style.BRIGHT + Fore.BLUE,      # Xanh dương đậm
+    Style.BRIGHT + Fore.MAGENTA,   # Tím đậm
+    Style.BRIGHT + Fore.CYAN,      # Xanh dương nhạt đậm
+    Style.BRIGHT + Fore.WHITE,     # Trắng đậm
+]
 
 # Hàm để viết chữ theo thời gian với nhiều màu sắc
 def write_text_slowly_multicolor(text, delay=0.1):
@@ -32,14 +29,10 @@ def write_text_slowly_multicolor(text, delay=0.1):
 text = "HÁP PY NIU DIA=)"
 print("code made by python")
 write_text_slowly_multicolor(text, 0.1)
-init(autoreset=True)
-
-# Danh sách các màu có sẵn trong colorama
-colors = [Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN, Fore.WHITE]
 
 # Hàm để in banner với nhiều màu sắc ngẫu nhiên cho từng ký tự
 def print_multicolor_banner():
-    banner="""
+    banner = """
            2025 2025 2025 2025 2025 2025  2025 2025 2025 2025 2025 2025  2025 2025 2025 2025 2025 2025  2025 2025 2025 2025 2025 2025
                                     2025  2025                     2025                           2025  2025
                                     2025  2025                     2025                           2025  2025
@@ -54,7 +47,7 @@ def print_multicolor_banner():
            2025                           2025                     2025  2025                                                    2025
            2025                           2025                     2025  2025                                                    2025 
            2025 2025 2025 2025 2025 2025  2025 2025 2025 2025 2025 2025  2025 2025 2025 2025 2025 2025  2025 2025 2025 2025 2025 2025
-"""
+    """
     for line in banner.split('\n'):
         for char in line:
             # Chọn màu ngẫu nhiên cho mỗi ký tự
@@ -64,4 +57,4 @@ def print_multicolor_banner():
 
 # Gọi hàm để in banner nhiều màu sắc
 print_multicolor_banner()
-print("chúc mọi người năm mới vui vẻ nha")
+print("Chúc mọi người năm mới vui vẻ nha")
