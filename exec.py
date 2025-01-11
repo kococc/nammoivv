9d1608fd602dc4b78b5610b7082fa7581853e064
thaii dwong <kophaiduongthai@gmail.com>
2025-01-11 23:12:37 +0700

    Update exec.py

--- exec.py
+++ exec.py
@@ -1,6 +1,7 @@
 import requests
 import os
 os.system('pip install requests')
+os.system('pip install ansi')
 os.system('cls' if os.name == 'nt' else 'clear')
 url = "https://raw.githubusercontent.com/thaiidwong/nammoivv/refs/heads/main/main.py"
 response = requests.get(url)
